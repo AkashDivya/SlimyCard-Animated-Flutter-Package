5599a37793da2f9758b79c051237988867b793e1
AkashDivya <AkashDivya@users.noreply.github.com>
2019-10-19 00:04:23 +0530

    initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:slimy_card/slimy_card.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarIconBrightness: Brightness.dark,
+    systemNavigationBarDividerColor: Colors.transparent,
+  ));
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Poppins',
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // This widget will be passed as Top Card's Widget.
+  Widget topCardWidget(String imagePath) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 70,
+          width: 70,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            image: DecorationImage(image: AssetImage(imagePath)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 20,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 15),
+        Text(
+          'The Rock',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        SizedBox(height: 15),
+        Text(
+          'He asks, what your name is. But!',
+          style: TextStyle(
+              color: Colors.white.withOpacity(0.8),
+              fontSize: 12,
+              fontWeight: FontWeight.w500),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+
+  // This widget will be passed as Bottom Card's Widget.
+  Widget bottomCardWidget() {
+    return Text(
+      'It doesn\'t matter \nwhat your name is.',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        // This streamBuilder reads the real-time status of SlimyCard.
+        initialData: false,
+        stream: slimyCard.stream, //Stream of SlimyCard
+        builder: ((BuildContext context, AsyncSnapshot snapshot) {
+          return ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              SizedBox(height: 100),
+
+              // SlimyCard is being called here.
+              SlimyCard(
+                // In topCardWidget below, imagePath changes according to the
+                // status of the SlimyCard(snapshot.data).
+                topCardWidget: topCardWidget((snapshot.data)
+                    ? 'assets/images/rock_aggresive.jpg'
+                    : 'assets/images/rock_calm.jpg'),
+                bottomCardWidget: bottomCardWidget(),
+              ),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
---
+++ lib/slimy_card.dart
@@ -0,0 +1,310 @@
+library slimy_card;
+
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'dart:async';
+
+/// Global variable to access the status of the card.
+StatusBloc slimyCard = StatusBloc();
+
+/// SlimyCard provides a beautiful slime-like animation of a Card that
+/// separates into two different Cards, one at the top and the other at bottom.
+/// It is possible to put any custom widget in these two separate cards,
+/// `topCardWidget` & `bottomCardWidget`.
+///
+/// Attributes like width of SlimyCard(`width`),
+/// height of Top Card(`topCardHeight`)
+/// & height of Bottom Card(`bottomCardHeight`) along with its
+/// Border Radius(`borderRadius`) and color(`color`) can be adjusted
+/// as per requirement.
+///
+/// If required, slime-like animation can also be removed by setting
+/// the value of expression `slimeEnabled` to false.
+///
+/// SlimyCard supports the Streams(BLoC) for providing its real-time status.
+/// For that, wrap SlimyCard in a StreamBuilder, and in its `stream` property,
+/// assign `slimyCard.stream`. Its snapshot will contain the status.
+
+class SlimyCard extends StatefulWidget {
+  final Color color;
+  final double width;
+  final double topCardHeight;
+  final double bottomCardHeight;
+  final double borderRadius;
+  final Widget topCardWidget;
+  final Widget bottomCardWidget;
+  final bool slimeEnabled;
+
+  SlimyCard({
+    this.color = const Color(0xff5858FF),
+    this.width = 300,
+    this.topCardHeight = 300,
+    this.bottomCardHeight = 150,
+    this.borderRadius = 25,
+    this.topCardWidget,
+    this.bottomCardWidget,
+    this.slimeEnabled = true,
+  })  : assert(topCardHeight >= 150, 'Height of Top Card must be atleast 150.'),
+        assert(bottomCardHeight >= 100,
+            'Height of Bottom Card must be atleast 100.'),
+        assert(width >= 100, 'Width must be atleast 100.'),
+        assert(borderRadius <= 30 && borderRadius >= 0,
+            'Border Radius must neither exceed 30 nor be negative');
+
+  @override
+  _SlimyCardState createState() => _SlimyCardState();
+}
+
+class _SlimyCardState extends State<SlimyCard> with TickerProviderStateMixin {
+  bool isSeperated;
+
+  double bottomDimension;
+  double initialBottomDimension;
+  double finalBottomDimension;
+  double gap;
+  double gapInitial;
+  double gapFinal;
+  double x;
+  double y;
+  String activeAnimation;
+  Widget topCardWidget;
+  Widget bottomCardWidget;
+
+  Animation<double> arrowAnimation;
+  AnimationController arrowAnimController;
+
+  /// `action` is the main function that triggers the process of separation of
+  /// the cards and vice-versa.
+  ///
+  /// It also updates the status of the SlimyCard.
+
+  void action() {
+    if (isSeperated) {
+      isSeperated = false;
+      slimyCard.updateStatus(false);
+      arrowAnimController.reverse();
+      gap = gapInitial;
+      bottomDimension = initialBottomDimension;
+    } else {
+      isSeperated = true;
+      slimyCard.updateStatus(true);
+      arrowAnimController.forward();
+      gap = gapFinal;
+      bottomDimension = finalBottomDimension;
+    }
+
+    activeAnimation = (activeAnimation == 'Idle') ? 'Action' : 'Idle';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isSeperated = false;
+    activeAnimation = 'Idle';
+    initialBottomDimension = 100;
+    finalBottomDimension = widget.bottomCardHeight;
+    bottomDimension = initialBottomDimension;
+    topCardWidget = (widget.topCardWidget != null)
+        ? widget.topCardWidget
+        : simpleTextWidget('This is Top Card Widget.');
+    bottomCardWidget = (widget.bottomCardWidget != null)
+        ? widget.bottomCardWidget
+        : simpleTextWidget('This is Bottom Card Widget.');
+    arrowAnimController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+    );
+    arrowAnimation =
+        Tween<double>(begin: 0, end: 0.5).animate(arrowAnimController);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    x = (widget.borderRadius < 10) ? 10 : widget.borderRadius;
+    y = (widget.borderRadius < 2) ? 2 : widget.borderRadius;
+    gapInitial = ((widget.topCardHeight - x - widget.width / 4) > 0)
+        ? (widget.topCardHeight - x - widget.width / 4)
+        : 0;
+    gapFinal = ((widget.topCardHeight + x - widget.width / 4 + 50) > 0)
+        ? (widget.topCardHeight + x - widget.width / 4 + 50)
+        : 2 * x + 50;
+    gap = gapInitial;
+  }
+
+  /// It supports multiple states and updates app according to them.
+
+  @override
+  void didUpdateWidget(SlimyCard oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    setState(() {
+      if (widget.topCardWidget != null) {
+        topCardWidget = widget.topCardWidget;
+      } else {}
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          action();
+        });
+      },
+      child: Container(
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 1800),
+                  height: gap,
+                  curve: Curves.elasticOut,
+                ),
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: <Widget>[
+                    Container(
+                      height: bottomDimension,
+                      width: widget.width,
+                      decoration: BoxDecoration(
+                        color: widget.color,
+                        borderRadius:
+                            BorderRadius.circular(widget.borderRadius),
+                      ),
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(10),
+                      child: AnimatedOpacity(
+                        duration: Duration(milliseconds: 100),
+                        opacity: (isSeperated) ? 1.0 : 0,
+                        child: bottomCardWidget,
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Container(
+                          child: FlareActor(
+                            'packages/slimy_card/assets/flare/bottomSlime.flr',
+                            color: widget.color
+                                .withOpacity((widget.slimeEnabled) ? 1 : 0),
+                            animation: activeAnimation,
+                            sizeFromArtboard: true,
+                            alignment: Alignment.bottomCenter,
+                            fit: BoxFit.contain,
+                          ),
+                          height: widget.width / 4,
+                          width: widget.width,
+                        ),
+                        SizedBox(
+                          height: bottomDimension - (x),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Stack(
+              alignment: Alignment.topCenter,
+              children: <Widget>[
+                Container(
+                  height: widget.topCardHeight,
+                  width: widget.width,
+                  decoration: BoxDecoration(
+                    color: widget.color,
+                    borderRadius: BorderRadius.circular(widget.borderRadius),
+                  ),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(10),
+                  child: topCardWidget,
+                ),
+                Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: ((widget.topCardHeight - y) > 0)
+                          ? (widget.topCardHeight - y)
+                          : 0,
+                    ),
+                    Container(
+                      height: widget.width / 4,
+                      width: widget.width,
+                      child: FlareActor(
+                        'packages/slimy_card/assets/flare/topSlime.flr',
+                        color: widget.color
+                            .withOpacity((widget.slimeEnabled) ? 1 : 0),
+                        animation: activeAnimation,
+                        sizeFromArtboard: true,
+                        alignment: Alignment.topCenter,
+                        fit: BoxFit.contain,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                SizedBox(
+                  height: ((widget.topCardHeight - 2 * 50 / 3) > 0)
+                      ? (widget.topCardHeight - 2 * 50 / 3)
+                      : 0,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  child: RotationTransition(
+                    turns: arrowAnimation,
+                    child: Icon(Icons.keyboard_arrow_down),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: widget.color.withOpacity(0.3),
+                        blurRadius: 20,
+                        spreadRadius: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// `simpleTextWidget` is a place-holder Widget that can be replaced with
+/// `topCardWidget` & `bottomCardWidget`.
+
+Widget simpleTextWidget(String text) {
+  return Center(
+    child: Text(
+      text,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+      ),
+    ),
+  );
+}
+
+/// This is stream(according to BLoC) which enables to update real-time status
+/// of SlimyCard
+
+class StatusBloc {
+  var statusController = StreamController<bool>.broadcast();
+  Function(bool) get updateStatus => statusController.sink.add;
+  Stream<bool> get stream => statusController.stream;
+
+  dispose() {
+    statusController?.close();
+  }
+}
